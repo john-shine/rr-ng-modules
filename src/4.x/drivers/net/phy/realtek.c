2ff02fc63b68ce78fe8037c6a3dcd8cda79db725
Ing <wjz304@qq.com>
2023-06-10 15:46:19 +0800

    fix realtek error

--- src/4.x/drivers/net/phy/realtek.c
+++ src/4.x/drivers/net/phy/realtek.c
@@ -124,7 +124,10 @@ static struct phy_driver realtek_drvs[] = {
 		.flags          = PHY_HAS_INTERRUPT,
 		.config_aneg    = &genphy_config_aneg,
 		.read_status    = &genphy_read_status,
+#if defined(CONFIG_SYNO_MDIOBUS)
+#else
 		.driver         = { .owner = THIS_MODULE,},
+#endif
 	}, {
 		.phy_id		= 0x001cc912,
 		.name		= "RTL8211B Gigabit Ethernet",
@@ -135,7 +138,10 @@ static struct phy_driver realtek_drvs[] = {
 		.read_status	= &genphy_read_status,
 		.ack_interrupt	= &rtl821x_ack_interrupt,
 		.config_intr	= &rtl8211b_config_intr,
+#if defined(CONFIG_SYNO_MDIOBUS)
+#else
 		.driver		= { .owner = THIS_MODULE,},
+#endif
 	}, {
 		.phy_id		= 0x001cc914,
 		.name		= "RTL8211DN Gigabit Ethernet",
@@ -148,7 +154,10 @@ static struct phy_driver realtek_drvs[] = {
 		.config_intr	= rtl8211e_config_intr,
 		.suspend	= genphy_suspend,
 		.resume		= genphy_resume,
+#if defined(CONFIG_SYNO_MDIOBUS)
+#else
 		.driver		= { .owner = THIS_MODULE,},
+#endif
 	}, {
 		.phy_id		= 0x001cc915,
 		.name		= "RTL8211E Gigabit Ethernet",
@@ -161,7 +170,10 @@ static struct phy_driver realtek_drvs[] = {
 		.config_intr	= &rtl8211e_config_intr,
 		.suspend	= genphy_suspend,
 		.resume		= genphy_resume,
+#if defined(CONFIG_SYNO_MDIOBUS)
+#else
 		.driver		= { .owner = THIS_MODULE,},
+#endif
 	}, {
 		.phy_id		= 0x001cc916,
 		.name		= "RTL8211F Gigabit Ethernet",
@@ -175,7 +187,10 @@ static struct phy_driver realtek_drvs[] = {
 		.config_intr	= &rtl8211f_config_intr,
 		.suspend	= genphy_suspend,
 		.resume		= genphy_resume,
+#if defined(CONFIG_SYNO_MDIOBUS)
+#else
 		.driver		= { .owner = THIS_MODULE },
+#endif
 	},
 };
 
